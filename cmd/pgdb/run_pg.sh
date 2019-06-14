7cd5a9e9586de3693f3f6a6808e2f9835d06acf7
Michael Mikhalevich <michael.mikhalevich@gmail.com>
2019-06-15 02:18:56 +0300

    run pg database script was corrected

--- cmd/pgdb/run_pg.sh
+++ cmd/pgdb/run_pg.sh
@@ -1,1 +1,2 @@
-docker run --name test-postgres -e POSTGRES_PASSWORD=123456 -e POSTGRES_DB=auth -e POSTGRES_USER=postgres -d -p 5432:5432 postgres
+docker run --rm --name test-postgres -e POSTGRES_PASSWORD=123456 -e POSTGRES_DB=auth -e POSTGRES_USER=postgres -d -p 5432:5432 postgres
+
